0971c341046cedd197e460c1af44e30ad798f1a5
Moses-20 <mosesgameli20@gmail.com>
2020-12-19 15:54:05 +0000

    built ecommerce user interface with iOS look

---
+++ android/app/src/main/kotlin/com/example/flu_ios_design/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flu_ios_design
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:cupertino_store/model/app_state_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+
+import 'app.dart';
+import 'model/app_state_model.dart';
+
+void main() {
+  return runApp(
+    ChangeNotifierProvider<AppStateModel>(
+      create: (_) => AppStateModel()..loadProducts(),
+      child: CupertinoStoreApp(),
+    ),
+  );
+}
